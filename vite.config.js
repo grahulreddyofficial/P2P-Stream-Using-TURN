d32cfda0176db54460715b2d1e370966cd7c51b7
G Rahul Reddy <grahulreddyofficial@gmail.com>
2026-02-01 21:57:47 +0530

    Changes Made

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/P2P-Stream-Using-TURN",
   server: {
     port: 3000,
     open: true,
