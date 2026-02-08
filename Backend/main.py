ec089a9bc69a2e80888a307c35142f9de50743b4
G Rahul Reddy <grahulreddyofficial@gmail.com>
2026-02-08 12:18:26 +0530

    Added Signaling Server

--- Backend/main.py
+++ Backend/main.py
@@ -4,6 +4,9 @@ from fastapi import FastAPI, HTTPException, File, Form, UploadFile
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 
+import mysql
+import mysql.connector
+
 import time
 import hmac
 import hashlib
@@ -29,6 +32,9 @@ app.add_middleware(
 TURN_SECRET = "SUPER_LONG_RANDOM_SECRET_KEY"
 TURN_URL = "turn:turn.yoursite.com:3478?transport=udp"
 
+class SignalData(BaseModel):
+    data: str
+
 def generate_turn_credentials(user_id: str):
     expiry = int(time.time()) + 3600  # 1 hour
     username = f"{expiry}:{user_id}"
@@ -47,6 +53,84 @@ def generate_turn_credentials(user_id: str):
         "credential": password
     }
 
+def get_db():
+    return mysql.connector.connect(
+        host="localhost",
+        user="Username",
+        password="Your_Password",
+        database="signaling_db",
+        port="3306"
+    )
+
 @app.get("/turn-credentials")
 def get_turn():
     return generate_turn_credentials("webuser")
+
+@app.post("/push-offer/{ucode}")
+def push_offer(ucode: str, body: SignalData):
+    con = get_db()
+    cursor = con.cursor()
+
+    try:
+        cursor.execute(
+            "INSERT INTO signalling_db (ucode, offer) VALUES (%s, %s)",
+            (ucode, body.data)
+        )
+        con.commit()
+    finally:
+        cursor.close()
+        con.close()
+
+    return {"db_push_status": "Offer pushed successfully"}
+
+@app.get("/get-offer/{ucode}")
+def get_offer(ucode: str):
+    con = get_db()
+    cursor = con.cursor()
+
+    try:
+        cursor.execute(
+            "SELECT offer FROM signalling_db WHERE ucode = %s",
+            (ucode,)
+        )
+        result = cursor.fetchone()
+    finally:
+        cursor.close()
+        con.close()
+
+    return {"offer": result[0] if result else None}
+
+
+@app.post("/push-answer/{ucode}")
+def push_answer(ucode: str, body: SignalData):
+    con = get_db()
+    cursor = con.cursor()
+
+    try:
+        cursor.execute(
+            "INSERT INTO signalling_db (ucode, answer) VALUES (%s, %s)",
+            (ucode, body.data)
+        )
+        con.commit()
+    finally:
+        cursor.close()
+        con.close()
+
+    return {"db_push_status": "Answer pushed successfully"}
+
+@app.get("/get-answer/{ucode}")
+def get_answer(ucode: str):
+    con = get_db()
+    cursor = con.cursor()
+
+    try:
+        cursor.execute(
+            "SELECT answer FROM signalling_db WHERE ucode = %s",
+            (ucode,)
+        )
+        result = cursor.fetchone()
+    finally:
+        cursor.close()
+        con.close()
+
+    return {"answer": result[0] if result else None}
